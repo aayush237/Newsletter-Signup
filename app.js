bf731bc4a1a4dca39caa97dc72a9dfb356acab16
Aayush Soni <110170691+aayush237@users.noreply.github.com>
2022-12-11 23:15:25 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ const bodyParser = require("body-parser");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-var list = config.LIST_ID;
-var API = config.API_ID;
-
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/signup.html");
 })
@@ -32,11 +29,11 @@ app.post("/", function(req, res){
     };
     const jsonData = JSON.stringify(data);
 
-    const url = "https://us13.api.mailchimp.com/3.0/lists/" + list;
+    const url = "https://us13.api.mailchimp.com/3.0/lists/dd83ba3077";
     const options = {
         method: "POST",
-        auth: "aayush237:" + API
-    }; 
+        auth: "aayush237:9120693f5b5cb14c64be78b393ceae97-us13"
+    } 
 
     const request = https.request(url, options, function(response){
         
@@ -63,3 +60,6 @@ app.listen(process.env.PORT || 3000, function(){
     console.log("Server is running at port 3000");
 });
 
+
+// List ID : dd83ba3077
+// API Key : 9120693f5b5cb14c64be78b393ceae97-us13
